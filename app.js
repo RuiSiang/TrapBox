9f8aefbe8e40a5a197a8b6ae1416a9725c6bbe05
RuiSiang <ruisiang2000@gmail.com>
2022-07-29 16:03:39 +0800

    basic code

---
+++ app.js
@@ -0,0 +1,48 @@
+const Koa = require('koa')
+const app = new Koa();
+
+const styles = `
+<style>
+body{
+  padding: 4rem;
+  background-color: #eee;
+}
+.wrap{
+  padding: 4rem;
+  background-color: white;
+}
+h1{
+  text-align: center;
+}
+</style>
+`
+
+app.use(ctx => {
+  const headers = Object.entries(ctx.headers || {}).map(([key, val]) => `<li>${key}=${val}</li>`).join('')
+  const query = Object.entries(ctx.query || {}).map(([key, val]) => `<li>${key}=${val}</li>`).join('')
+  const params = Object.entries(ctx.params || {}).map(([key, val]) => `<li>${key}=${val}</li>`).join('')
+  const body = ctx.request.body
+  const html = `
+  <!DOCTYPE html>
+  <html>
+  <header>
+  ${styles}
+  </header>
+  <body>
+  <div class="wrap">
+  <h1>TrapBox</h1>
+  <h2>Headers</h2>
+  <ul>${headers || ''}</ul>
+  <h2>Params</h2>
+  <ul>${params || ''}</ul>
+  <h2>Query</h2>
+  <ul>${query || ''}</ul>
+  <h2>Body</h2>
+  <div>${body || ''}</div>
+  </div>
+  </body>
+  </html>`
+  ctx.body = html
+})
+
+app.listen(parseInt('80' || pprocess.env.PORT))
